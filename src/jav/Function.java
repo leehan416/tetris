dd01c9b2381ecb73f87868db9ef6fc687a4d92c9
Whal3 <35258105+leehan416@users.noreply.github.com>
2021-03-04 15:04:21 +0900

    .

--- src/jav/Function.java
+++ src/jav/Function.java
@@ -1,5 +1,6 @@
 package jav;
 
+//https://itlove.tistory.com/254 z키입력
 public class Function {
 	public static Function instance = new Function();
 
@@ -8,12 +9,22 @@ public class Function {
 	}
 
 	public void ZeroSet() {
-
 		for (int y = 0; y < 10; y++) {
 			for (int x = 0; x < 10; x++) {
 				DataBase.instance.slot[y][x] = 0;
 			}
 		}
+	}
+
+	public void MovingDel() {
+
+		for (int y = 0; y < 10; y++) {
+			for (int x = 0; x < 10; x++) {
+				if (DataBase.instance.slot[y][x] != 2)
+					DataBase.instance.slot[y][x] = 0;
+			}
+		}
+
 	}
 
 	public void Check() { // 10개 모였는지 확인하는 함수
@@ -34,76 +45,70 @@ public class Function {
 	public void BlockSet(int val, int x, int y) { // 블럭 세팅하는 함수
 		// ToDoList : 가장 아래있는 블럭으로 세팅하는 함수로 다시 짜야함
 		switch (val) {
-		case 0: // ㅣ 자 블럭
-			if (DataBase.instance.angle & 2 == 1) { // 0 or 90
+		case 0: { // ㅣ 자 블럭
+			if ((DataBase.instance.angle & 2) == 1) { // 0 or 180
 				DataBase.instance.slot[y - 2][x] = 1;
 				DataBase.instance.slot[y - 1][x] = 1;
 				DataBase.instance.slot[y][x] = 1;
 				DataBase.instance.slot[y + 1][x] = 1;
-			} else {
+			} else { // 90 or 270
 				DataBase.instance.slot[y][x + 1] = 1;
 				DataBase.instance.slot[y][x + 2] = 1;
 				DataBase.instance.slot[y][x] = 1;
 				DataBase.instance.slot[y][x - 1] = 1;
 			}
 			break;
-		case 1: // ㅁ 자 블럭
+		}
+		case 1: {// ㅁ 자 블럭
 			DataBase.instance.slot[y][x] = 1;
 			DataBase.instance.slot[y][x + 1] = 1;
 			DataBase.instance.slot[y + 1][x] = 1;
 			DataBase.instance.slot[y + 1][x + 1] = 1;
 			break;
-		case 2: // ㄱ 자 블럭
-			switch (angle) {
-			case 0:
-				DataBase.instance.slot[y][x-3] = 1;
-				DataBase.instance.slot[y][x -2] = 1;
-				DataBase.instance.slot[y + 1][x] = 1;
-				DataBase.instance.slot[y][x + 2] = 1;
-				break;
-			case 1:
-				DataBase.instance.slot[y][x] = 1;
-				DataBase.instance.slot[y][x + 1] = 1;
-				DataBase.instance.slot[y + 1][x] = 1;
-				DataBase.instance.slot[y][x + 2] = 1;
-				break;
-			case 2:
-
-				break;
-			case 3:
+		}
+		case 2: {// ㄱ 자 블럭
+			switch (DataBase.instance.angle) {
+			case 0: {
 
 				break;
 			}
-			DataBase.instance.slot[y][x] = 1;
-			DataBase.instance.slot[y][x + 1] = 1;
-			DataBase.instance.slot[y][x + 2] = 1;
-			DataBase.instance.slot[y + 1][x + 2] = 1;
+			case 1: {
+
+				break;
+			}
+			case 2: {
+
+				break;
+			}
+			case 3: {
+
+				break;
+			}
+			}
+
 			break;
-		case 3: // ㅗ 자 블럭
-			DataBase.instance.slot[y][x] = 1;
-			DataBase.instance.slot[y + 1][x - 1] = 1;
-			DataBase.instance.slot[y + 1][x] = 1;
-			DataBase.instance.slot[y + 1][x + 1] = 1;
+		}
+		case 3: {// ㅗ 자 블럭
+
 			break;
-		case 4: // ㄹ 자 블럭
-			DataBase.instance.slot[y][x] = 1;
-			DataBase.instance.slot[y + 1][x] = 1;
-			DataBase.instance.slot[y + 1][x + 1] = 1;
-			DataBase.instance.slot[y + 2][x + 1] = 1;
+		}
+		case 4: {// ㄹ 자 블럭
+
 			break;
+		}
 		}
 
 	}
 
 	public void Down() {
-		ZeroSet(); // 설치된 블럭은 제거 안하는 방향으로 변경할것
+		MovingDel(); // 설치된 블럭은 제거 안하는 방향으로 변경할것
 		try {
 			BlockSet(DataBase.instance.val, DataBase.instance.x, ++DataBase.instance.y);
 		} catch (Exception e) {
 			BlockSet(DataBase.instance.val, DataBase.instance.x, --DataBase.instance.y);
 			OnEnter();
-			return;
 		}
+	}
 
 	public void OnEnter() { // 충돌(접촉)시
 		DataBase.instance.x = 4;
--- src/jav/main.java
+++ src/jav/main.java
@@ -1,29 +1,93 @@
 package jav;
 
+import javax.swing.*;
+import java.awt.event.*;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class main {
-	
+
 	static void print() { // 출력 함수
+		DataBase.instance.printData = "";
 		for (int h = 0; h < DataBase.instance.slot.length; h++) {
 			for (int w = 0; w < DataBase.instance.slot.length; w++) {
 				if (DataBase.instance.slot[h][w] > 0) {
-					System.out.print("■");
+					DataBase.instance.printData += "■";
 				} else if (DataBase.instance.slot[h][w] == 0) {
-					System.out.print("□");
+					DataBase.instance.printData += "□";
 				}
 			}
-			System.out.println("");
+			DataBase.instance.printData += "\n";
 		}
-		System.out.println("");
+		DataBase.instance.printData += "\n";
 	}
 
 	public static void main(String[] args) {
 		Function.instance.ZeroSet();
 		Function.instance.BlockSet(DataBase.instance.val, DataBase.instance.x, DataBase.instance.y);
+
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// f.setLocationRelativeTo(null); // 1 //자동으로 화면 중앙에 배치
+		f.setSize(500, 500);
+		f.setVisible(true);
+
+		JLabel label = new JLabel();
+		label.setBounds(0, 0, 500, 500);
+		f.remove(label);
+		label.setText(DataBase.instance.printData);
+		f.add(label);
+
+		class key implements KeyListener {
+
+			public void keyPressed(KeyEvent e) {
+
+				if (e.getKeyCode() == 37) {
+					f.remove(label);
+					label.setText("왼족 누름");
+					f.add(label);
+				}
+				if (e.getKeyCode() == 38) {
+					f.remove(label);
+					label.setText("위 누름");
+					f.add(label);
+				}
+				if (e.getKeyCode() == 39) {
+					f.remove(label);
+					label.setText("오른 누름");
+					f.add(label);
+				}
+				if (e.getKeyCode() == 40) {
+					f.add(label);
+					label.setText("아래 누름");
+					f.add(label);
+				}
+			}
+
+			public void keyReleased(KeyEvent e) {
+			}
+
+			public void keyTyped(KeyEvent e) {
+			}
+
+		}
+
+		f.setVisible(true);
+		f.addKeyListener(new key());
+		f.setFocusable(true);
+
 		while (true) {
 			if (DataBase.instance.roof > 20) { // test
 				break;
 			}
 			print();
+			f.remove(label);
+			label.setText(DataBase.instance.printData);
+			f.add(label);
 			try {
 				Thread.sleep(1000);
 			} catch (Exception e) {
@@ -32,5 +96,6 @@ public class main {
 			Function.instance.Down();
 			DataBase.instance.roof++;
 		}
+
 	}
 }
